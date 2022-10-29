19db291ad2bdaa5ce6916217a27f80f74091317c
dxhd <dmitrix01@gmail.com>
2022-10-29 12:11:08 +0500

    Initialization commit

---
+++ src/main/java/com/tinkoff/skipper/SkipperApplication.java
@@ -0,0 +1,13 @@
+package com.tinkoff.skipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipperApplication.class, args);
+	}
+
+}
