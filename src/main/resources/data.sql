d6bb10a19d7bd15f2545a2a6bc8fd9cdf3425e49
Egor <ejs.defolt@gmail.com>
2022-11-23 19:54:44 +0500

    test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,39 @@
+insert into users (description, email, phone_number, password)
+values ('some desc1', 'test1@mail.ru', 'dxhd', 'passss');
+insert into users (description, email, phone_number, password)
+values ('some desc1', 'test2@mail.ru', 'jjk', 'pass21s');
+insert into users (description, email, phone_number, password)
+values ('some desc1', 'test3@mail.ru', 'chopper', 'pa23ssss');
+insert into users (description, email, phone_number, password)
+values ('some desc1', 'test4@mail.ru', 'cactus', 'passs432s');
+
+INSERT into mentor_info (education, subjects, user_id)
+VALUES ('susu4', 'programming', 2);
+INSERT into mentor_info (education, subjects, user_id)
+VALUES ('susu4', 'cooking', 3);
+
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('FINISHED', 1, 1, '12-10-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('CANCELLED', 1, 1, '12-09-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('FINISHED', 1, 1, '12-09-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('FINISHED', 1, 1, '12-01-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('PLANNED', 1, 1, '13-10-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('IN_PROGRESS', 1, 1, '12-11-2022');
+
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('CANCELLED', 1, 2, '12-08-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('FINISHED', 1, 2, '15-09-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('PLANNED', 1, 2, '11-11-2022');
+INSERT INto lessons (status, mentee_id, mentor_id, date_of_lesson)
+values ('IN_PROGRESS', 1, 2, '12-10-2022');
+
+
+
+
