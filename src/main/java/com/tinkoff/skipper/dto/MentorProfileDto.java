decce9adbcdbad48de3ac1193e704d7078b3a5a1
Egor <ejs.defolt@gmail.com>
2022-11-24 19:04:35 +0500

    feat: add more fields to mentor profile dto

--- src/main/java/com/tinkoff/skipper/dto/MentorProfileDto.java
+++ src/main/java/com/tinkoff/skipper/dto/MentorProfileDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import com.tinkoff.skipper.entity.MentorInfoEntity;
 
-import java.time.OffsetDateTime;
+import java.time.LocalDate;
 import java.math.BigDecimal;
 
 import com.tinkoff.skipper.entity.UserEntity;
@@ -18,24 +18,26 @@ public class MentorProfileDto {
     private StatsDto stats;
     private String reviews;
     private String lessonTemplates;
-    private OffsetDateTime timeZone;
+    private BigDecimal timeZone;
     private String speciality;
     private BigDecimal rating;
     private LocalDate registrationDate;
+    private Long id;
 
     // FIXME: merge model and dto
     public static MentorProfileDto toModel(UserEntity userEntity, MentorInfoEntity mentorEntity, StatsDto stats)
     {
         MentorProfileDto model = new MentorProfileDto();
         model.setUsername(mentorEntity.getUsername());
-        model.setUserpicture(userEntity.getUserPicture());
         model.setDescription(mentorEntity.getDescription());
+        model.setUserPicture(userEntity.getUserPicture());
         model.setStudentNumber(mentorEntity.getStudentNumber());
         model.setStats(stats);
         model.setTimeZone(userEntity.getTimeZone());
         model.setSpeciality(mentorEntity.getSpeciality());
         model.setRating(mentorEntity.getRating());
-        model.setRegistrationDate(userEntity.getCreatedAt());
+         model.setRegistrationDate(userEntity.getCreatedAt());
+        model.setId(userEntity.getId());
         return model;
     }
 }
