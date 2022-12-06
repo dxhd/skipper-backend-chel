55dc5e267385837f1b67a5aa11eb2f7afbcfb308
dxhd <dmitrix01@gmail.com>
2022-12-07 00:33:24 +0500

    deleted data1.sql

--- src/main/java/com/tinkoff/skipper/service/UserService.java
+++ src/main/java/com/tinkoff/skipper/service/UserService.java
@@ -8,9 +8,11 @@ import com.tinkoff.skipper.exception.SkipperNotFoundException;
 import com.tinkoff.skipper.repository.RoleRepo;
 import com.tinkoff.skipper.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
