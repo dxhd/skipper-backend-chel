f0f9ad50aefdf352a5cc4438090841bce6540165
dxhd <dmitrix01@gmail.com>
2022-12-19 22:50:50 +0500

    fix: stats issue

--- src/main/java/com/tinkoff/skipper/service/MenteeService.java
+++ src/main/java/com/tinkoff/skipper/service/MenteeService.java
@@ -5,17 +5,21 @@ import com.tinkoff.skipper.exception.SkipperBadRequestException;
 import com.tinkoff.skipper.repository.LessonRepo;
 import com.tinkoff.skipper.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MenteeService {
 
     private final LessonRepo lessonRepo;
     private final UserRepo userRepo;
 
     public MenteeProfileDto getMenteeUserInfoById(Long id) {
-        lessonRepo.updateStatus();
+        try {
+            lessonRepo.updateStatus();
+        } catch (Exception e) { log.error(e.getMessage());}
         return MenteeProfileDto.toModel(
                 userRepo.findById(id).orElseThrow(
                     () -> new SkipperBadRequestException(" Пользователь не найден")
